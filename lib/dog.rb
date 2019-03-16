2866fd44800f0f8fa610b9429d11324d3026793e
Ming Duan <11203duan@gmail.com>
2019-03-16 23:01:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+@@all = []
+
+def self.clear_all
+  @@all = []
+end
+
+def self.all
+  @@all.each {|x| puts x.name}
+end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+def name
+  @name
+end
+
+
+end
